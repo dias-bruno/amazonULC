7e9c1d5e1c8c3ec44be87ee2a3e86a1af3e5e61d
Antonio Paez <paezha@gmail.com>
2022-11-21 11:27:25 -0500

    Added script for hex creator, added code for badges, added author in description, started with documentation of package

--- R/amazonULC-package.R
+++ R/amazonULC-package.R
@@ -4,3 +4,12 @@
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
+
+#' amazonULC: A Data Package with Urban Land Cover Classifications for a Selection of Cities in the Brazilian Amazon
+#'
+#' @docType package
+#' @name amazonULC-package
+#' @author Bruno Dos Santos, INPE \email{bruno.santos@@inpe.br}
+#' @author Antonio Paez, School of Earth, Environment and Society, McMaster University \email{paezha@@mcmaster.ca}
+#' @references \url{https://github.com/dias-bruno/amazonULC}
+NULL
